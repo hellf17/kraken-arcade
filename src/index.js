60601052432affb7c9c5358967effb704449828d
Monolith <88285936+0xMonolith@users.noreply.github.com>
2023-08-09 15:41:37 -0700

    Update the index.js file to load your assets into Phaser's cache
    +Loading all assets, with nice code structure and separation of concerns within the index.js file
    
    +Webpack live dev server implemented
    
    -Need to change the README file to include instructions on running the webpack live dev server
    
    ^(this live dev server is able to be run and hot reloaded upon any file changes in your IDE)
    
    -Need to change all animations of the kraken to spritesheets
    
    -Need to review some code we have (that is closed source obviously) on how to pull metadata and "build" a user's pseudo-kraken NFT, so that they are able to use whichever kraken they own within the game
    
    -Need to integrate the game logic and UI into Phaser using JS (Preferably Typescript, but I will do this later,my goal is to get a live version working asap, since I do also have to work on the MMO, and many other CEO things LMFAO)
    
    -Add more mechanics, buffs, balance and test the game, get it working as an integration into the Community page on the Trident.game website
    
    -Add better audio tracks (That are not copyright kek)
    
    -Add better animations and art assets
     (I have to make these, which are not difficult, but trying to get the game into a better state before changing the "bells and whistles")
    
    Godspeed laddy! <3

--- src/index.js
+++ src/index.js
@@ -10,7 +10,59 @@ class MyGame extends Phaser.Scene
 
     preload ()
     {
+
+        //Load all audio, images, spritesheets, etc
+        //Separate them into different functions, which we will make below the preload function
+        //this.loadImages()
+        //this.loadAudio()
+        //this.loadSpritesheets()
+        //this.loadTilemaps()
+
+        this.loadImages();
+
+    }
+
+    loadImages(){
+
+        //test logo that is in the Phaser template repository
         this.load.image('logo', logoImg);
+        //Load all the assets/images/still-image in the images folder
+        //Backgrounds
+        this.load.image('background-space-desert', 'assets/images/still-image/background1.png');
+        this.load.image('background-space-purple', 'assets/images/still-image/background2.png');
+        //Enemies
+        this.load.image('enemy1', 'assets/images/still-image/enemies/enemy_1.png');
+        this.load.image('enemy2', 'assets/images/still-image/enemies/enemy_2.png');
+        this.load.image('enemy3', 'assets/images/still-image/enemies/enemy_3.png');
+        //Items (Called "buffs")
+        this.load.image('item1', 'assets/images/still-image/items/buff1.png');
+        this.load.image('item2', 'assets/images/still-image/items/buff2.png');
+        this.load.image('item3', 'assets/images/still-image/items/buff3.png');
+        //Projectile(s)
+        this.load.image('projectile1', 'assets/images/still-image/projectiles/projectile.png');
+
+    }
+
+    loadAudio(){
+
+        //All audio should be in .WAV format, but this should be fine for now.
+        //Background Music
+        this.load.audio('background-music-1', 'assets/audio/background1.mp3');
+        this.load.audio('background-music-2', 'assets/audio/background2.mp3');
+        this.load.audio('background-music-3', 'assets/audio/background3.wav');
+        //Projectile Sound(s)
+        this.load.audio('projectile-sound-1', 'assets/audio/projectile.mp3');
+    }
+
+    loadSpritesheets(){
+
+        //Currently, we do not have any spritesheets.
+        //A spritesheet is a .png file that contains all the frames of an animation.
+        //We will need to create spritesheets for the player, enemies, projectiles, and everything else that has an animation.
+        //There are other ways to have animations, but spritesheets are generally what we use.
+
+        //There is code that we have in the pseudo-kraken game that loads spritesheets, from metadata that is pulled from the NFT smart contracts for pseudo-krakens.
+        //I will have to review how we did that, and then implement it here, if we want the player to use their own kraken(s) as their in-game character for the arcade-kraken shooter.
     }
       
     create ()
