4adabf10e53106548b1a83b5ccaf294984aa11e4
hellf <115511997+hellf17@users.noreply.github.com>
2023-08-24 14:26:08 -0300

    Added player movements to the player.js file

--- src/player.js
+++ src/player.js
@@ -1,9 +1,8 @@
-// Create player
-const createPlayer = (scene, screenX, screenY) => {
-    const player = scene.physics.add.sprite(screenX, screenY, 'player');
-    player.setScale(1.5);
-    createAnimations(scene);
-    return player;
+import * from Phaser as 'phaser'
+
+const playerType = {
+    Type1: 0,
+    Type2: 1
 };
 
 // Load player spritesheet
@@ -13,6 +12,46 @@ const loadPlayer = (scene) => {
         'src/assets/images/spritesheets/kraken-player/kraken1-idle-sheet.png',
         { frameWidth: 150, frameHeight: 150, spacing: 33 }
     );
+};
+
+class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, type) {
+        super(scene, x, y, type);
+        scene.add.existing(this);
+        scene.physics.world.enable(this);
+        this.setScale(1.5);
+
+        //Different types for holding Kraken or Mortis
+        switch (type) {
+            case playerType.Type1:
+                this.hitpoints = 5;
+                this.baseVelocity = 70;
+                this.xpTracker = 0;
+                break;
+            case playerType.Type2:
+                this.hitpoints = 4;
+                this.baseVelocity = 80;
+                this.xpTracker = 0;
+                break;
+
+        scene.physics.world.add(this);
+        }                
+
+    receiveDamage(damage) {
+        this.hitpoints -= damage;
+    
+        if (this.hitpoints <= 0) {
+            this.destroy();
+            }
+        }
+}
+
+// Create player
+const createPlayer = (scene, screenX, screenY, 'playerType') => {
+    const playerType = Type1; //need to implement the rest of the method for this to work
+    const player = Player(scene, screenX, screenY, playerType);
+    createAnimations(scene);
+    return player;
 };
 
 // Animate the player
@@ -25,5 +64,29 @@ const createAnimations = (scene) => {
         yoyo: true
     });
 };
+
+const movePlayer (scene, player) => {    
+    // Player movements
+    const baseVeloc = 150;
+    var multiVeloc = 1; //
+    let velocityX = 0;
+    let velocityY = 0;
+    
+    if (this.keys.left.isDown) {
+        velocityX = -baseVeloc * multiVeloc;
+    } 
+    if (this.keys.right.isDown) {
+        velocityX = baseVeloc * multiVeloc;
+    }
+    if (this.keys.down.isDown) {
+        velocityY = baseVeloc * multiVeloc;
+    } 
+    if (this.keys.up.isDown) {
+        velocityY = -baseVeloc * multiVeloc;
+    }
+      
+    // Set player velocity based on input
+    this.player.setVelocity(velocityX, velocityY);
+}
 
 export { createPlayer, loadPlayer, createAnimations };
